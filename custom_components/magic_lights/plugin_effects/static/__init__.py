f295a351342c5d8e61e77b85b9f825cca145d9b2
Ariel Ebersberger <ariel@ebersberger.io>
2025-04-04 19:22:09 +0000

    Refactor static plugin

--- custom_components/magic_lights/plugin_effects/static/__init__.py
+++ custom_components/magic_lights/plugin_effects/static/__init__.py
@@ -1,6 +1,12 @@
+import aiofiles
+import json
+import os
+from typing import Any
+import asyncio
+
 from homeassistant.components.light import ColorMode
 from custom_components.magic_lights.plugin_effects import Effect
-from .helpers.io_helper import load_presets
+
 
 CONFIG_SCHEMA = None
 PLUGIN_DOMAIN = "static"
@@ -9,79 +15,91 @@ PLUGIN_DESCRIPTION = "Simple static light scene."
 
 
 class Static(Effect):
-    def __init__(self, async_call_service, entities, conf):
+    def __init__(self, async_call_service, entities: list[str], conf: dict[str, Any]):
         super().__init__(async_call_service, entities, conf)
-
-        supported_scenes = [
-            "Bright",
-            "Energize",
-            "Concentrate",
-            "Read",
-            "Relax",
-            "Dimmed",
-            "Nightlight",
-        ]
 
         self.service_data: dict[str, dict] = {}
 
         if "preset" in self.conf:
-            if self.conf["preset"] not in supported_scenes:
-                self.log.error("Preset %s not supported")
-                return
+            selected_preset = self.conf["preset"]
+            self.magic.hass.loop.create_task(
+                self.async_preset_setup(selected_preset))
+        else:
+            self.service_data = conf
 
-            self.magic.hass.loop.create_task(self.async_preset_setup())
+    async def async_preset_setup(self, selected_preset: str):
+        # Load presets
+        presets = await load_presets()
+        selected_preset_conf = presets.get(selected_preset, None)
+        if selected_preset_conf is None:
+            self.log.error("Unknown preset '%s'", self.selected_preset)
+            return
 
-    async def async_preset_setup(self):
-        self.selected_preset = self.conf["preset"]
-        self.presets = await load_presets()
-
+        # Go through all entities
         for entity in self.entities:
             # Check if entity is light
             domain = entity.rsplit(".")[0]
             if domain != "light":
-                self.log.warn("Only Light Entites are supported currently.")
+                self.log.warning(
+                    "Only Light Entites are supported within preset scenes for '%s' plugin currently.", PLUGIN_NAME)
 
             # Get State and detect light type
             state = self.magic.hass.states.get(entity)
-            try:
-                light_type = state.attributes["supported_color_modes"]
-            except KeyError:
-                self.log.warn(
-                    "Entity %s does not specify its supported color modes", entity
+            entity_supported_modes: list[ColorMode] = state.attributes.get(
+                "supported_color_modes", [])
+
+            if len(entity_supported_modes) == 0:
+                self.log.warning(
+                    "Entity %s does not specify its supported color modes. Defaulting to 'ONOFF'/ON.", entity
                 )
+                self.service_data[entity] = {}
 
-            # Save function and service data
-            if ColorMode.ONOFF in light_type:
-                self.service_data.update(
-                    {entity: self.get_preset_value("onoff")})
+            if set(entity_supported_modes).isdisjoint(selected_preset_conf):
+                self.log.warning(
+                    "Entity %s does not support preset '%s' with modes [%s]. Defaulting to 'ONOFF'/ON.", entity, selected_preset, entity_supported_modes
+                )
+                self.service_data[entity] = {}
 
-            elif ColorMode.BRIGHTNESS in light_type:
-                self.service_data.update(
-                    {entity: self.get_preset_value("brightness")})
+            # Get the first supported mode
+            for scene_supported_mode in selected_preset_conf:
+                if scene_supported_mode in entity_supported_modes:
+                    self.log.debug(
+                        "Entity %s supports preset '%s' with mode '%s'", entity, selected_preset, scene_supported_mode
+                    )
+                    self.service_data[entity] = selected_preset_conf[scene_supported_mode]
+                    break
 
-            elif ColorMode.COLOR_TEMP in light_type:
-                self.service_data.update(
-                    {entity: self.get_preset_value("color_temp")})
-
-            elif ColorMode.XY in light_type:
-                self.service_data.update({entity: self.get_preset_value("xy")})
-
-        self.log.debug("Effect Static Initialized")
-
-    def get_preset_value(self, light_type: str):
-        if self.selected_preset not in self.presets[light_type]:
-            return "off"
-        else:
-            return self.presets[light_type][self.selected_preset]
+        self.log.debug("Effect Static Initialized. Preset: %s, Number of Entities: %s",
+                       selected_preset, str(len(self.service_data)))
 
     async def async_start(self):
-        self.log.debug(f"Static Effect started. \nConfig: {self.conf}")
+        self.log.debug("Static Effect started."
+                       f"\nConfig: {self.conf}"
+                       f"\nEntities: {self.entities}")
 
+        jobs = []
         for entity in self.entities:
             s_data = {"entity_id": entity}
+            conf_data: str | dict[str, str] = self.service_data.get(
+                entity, None)
 
-            if self.service_data[entity] == "off":
-                await self.async_call_service("light", "turn_off", s_data)
+            self.log.debug("Adjusting Entity: %s. Conf Data: %s",
+                           entity, conf_data)
+            if conf_data == "off":
+                jobs.append(
+                    self.async_call_service("light", "turn_off", s_data))
             else:
-                s_data.update(self.service_data[entity])
-                await self.async_call_service("light", "turn_on", s_data)
+                s_data.update(conf_data)
+                jobs.append(
+                    self.async_call_service("light", "turn_on", s_data))
+
+        await asyncio.gather(*jobs)
+
+
+async def load_presets() -> dict:
+    async with aiofiles.open(
+        os.path.join(os.path.dirname(__file__), "values.json"), mode="r"
+    ) as f:
+        values = await f.read()
+
+    return json.loads(values)
